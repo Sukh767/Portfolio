1340f719e649a586e8da63dfc56cd464114c574a
Sukharanjan Jana <117519344+Sukh767@users.noreply.github.com>
2024-11-23 07:32:15 +0530

    Update Navbar.jsx

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -40,7 +40,7 @@ export default function Navbar() {
     <nav className="fixed top-0 left-0 right-0 z-50 bg-white dark:bg-gray-800 shadow-md transition-all duration-300 ease-in-out">
       <div className="container mx-auto px-4 md:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
-          <h2 className="text-xl font-bold text-slate-700 dark:text-white"><Sukharanjan/Jana></h2>
+          <h2 className="text-xl font-bold text-slate-700 dark:text-white"><span><Sukharanjan/Jana></span></h2>
 
           {/* Desktop navbar */}
           <div className="hidden md:flex">
